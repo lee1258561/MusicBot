febaa0f73e934c6521bff95a3bd801757390513a
Leon Hsieh <sun82522@gmail.com>
2017-04-03 02:07:57 +0800

    Add Training data 1st version

---
+++ download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python data/drive_download.py 0B6HG80vOD3w7TXBfckVObDJMRFk data/Train_Intent
+python data/drive_download.py 0B6HG80vOD3w7Y1hsdmhIOGVGLW8 data/Train_POS
+python data/drive_download.py 0B6HG80vOD3w7YmpNQ0d0NXc0d3c data/Train_X
