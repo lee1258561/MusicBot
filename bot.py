08f873d28c6b029987146485a7c85bb4e6ba73be
Leon Hsieh <sun82522@gmail.com>
2017-04-11 04:38:19 +0000

    Integrate music to 'the silence bot' fb acount

--- bot.py
+++ bot.py
@@ -1,9 +1,17 @@
 import fbchat
-#subclass fbchat.Client and override required methods
-class EchoBot(fbchat.Client):
 
+from rnn_nlu import run_multi_task_rnn, data_utils
+from ontology import databaseAPI
+#subclass fbchat.Client and override required methods
+class fbBot(fbchat.Client):
     def __init__(self,email, password, debug=True, user_agent=None):
         fbchat.Client.__init__(self,email, password, debug, user_agent)
+
+        print('NLU initializing...')
+        self.NLU = run_multi_task_rnn.NLU_test()
+
+        print('database initializing...')
+        self.db = databaseAPI.Database()
 
     def on_message(self, mid, author_id, author_name, message, metadata):
         self.markAsDelivered(author_id, mid) #mark delivered
@@ -16,8 +24,14 @@ class EchoBot(fbchat.Client):
         if 'delta' in metadata and 'messageMetadata' in metadata['delta'] and 'threadKey' in metadata['delta']['messageMetadata'] and 'threadFbId' in metadata['delta']['messageMetadata']['threadKey']:
             ID = metadata['delta']['messageMetadata']['threadKey']['threadFbId']
         #if you are not the author, echo
-        if str(author_id) != str(self.uid):
-            self.send(author_id,message)
+        if str(author_id) != str(self.uid) and ID == None:
+            intent, pos = self.NLU.feed_sentence(message)
+            print(intent, pos)
+            slot = databaseAPI.build_slot(data_utils.naive_seg(message), pos)
+            print(slot)
+            _, sentence = self.db.given(slot)
+        
+            self.send(author_id,sentence)
 
-bot = EchoBot("email", "password")
+bot = fbBot("cotapocil@1rentcar.top", "silencefarmer")
 bot.listen()
