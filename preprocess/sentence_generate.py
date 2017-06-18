c114a294342c52071b10ae0329b442cbc69bca3f
Leon Hsieh <sun82522@gmail.com>
2017-06-19 00:09:33 +0800

    Generate playlist related templates

--- preprocess/sentence_generate.py
+++ preprocess/sentence_generate.py
@@ -16,12 +16,16 @@ and the artists, tracks, albums, genres data crawled from Spotify
 '''
 
 
+INTENTS = ['search', 'recommend','info','neutral', 'playlistCreate', 'playlistAdd', 'playlistPlay',
+           'playlistShow', 'playlistTrack','playlistSpotify']
+
 def opt_parse():
     parser = argparse.ArgumentParser(description=\
             'Slot & intent data generate')
     parser.add_argument('template',help='sentence template')
     parser.add_argument('data',help='artist-album-track json data')
     parser.add_argument('genre',help='genres')
+    parser.add_argument('playlist_names',help='playlist names')
     parser.add_argument('-output',default='Train',help='output filename prefix')
     parser.add_argument('--nb_per_template',default=100,type=int,\
             help='use 1 template sentences generate n times')
@@ -29,13 +33,13 @@ def opt_parse():
     return args
 
 
-def slot_sample(s=None,a=None,t=None,g=None,l=None):
-    slot = {'[s]':s,'[a]':a,'[t]':t,'[g]':g,'[l]':l}
+def slot_sample(s=None,a=None,t=None,g=None,p=None):
+    slot = {'[s]':s,'[a]':a,'[t]':t,'[g]':g,'[p]':p}
     return slot
 
 
-def fill_slot(X,POS,Intent,intent,template,s=None,a=None,t=None,g=None,l=None):
-    slot_map = slot_sample(s,a,t,g,l)
+def fill_slot(X,POS,Intent,intent,template,s=None,a=None,t=None,g=None,p=None):
+    slot_map = slot_sample(s,a,t,g,p)
     new_temp = io_utils.naive_seg(template)
     pos = [0 for i in range(len(new_temp))]
 
@@ -54,18 +58,18 @@ def fill_slot(X,POS,Intent,intent,template,s=None,a=None,t=None,g=None,l=None):
     return
 
 
-def fill_template(data_artist,data_sent,genres,args_output, intent,nb_per_template=100):
+def fill_template(data_artist,data_sent,genres, playlist_names, args_output, intent,nb_per_template=100):
     '''
         fill the given [...] slot of template sentences
         then store to file with prefix of args_output
     '''
     ### init
     data_sent = data_sent[data_sent.columns[0]].unique()
+    playlist_names = playlist_names[playlist_names.columns[0]].unique()
     X = [] # [[],[],...]
     POS = [] # [[],[],...]
     Intent = [] # []
     
-    intents = ['search', 'recommend','info','neutral']
     artists = [s for s in data_artist]
     tracks = []
     for s in data_artist:
@@ -79,19 +83,18 @@ def fill_template(data_artist,data_sent,genres,args_output, intent,nb_per_templa
         #intent = sent[0].decode('utf-8')
         template = sent.decode('utf-8')
         #print n,intent,template
-        if intent in intents:
+        if intent in INTENTS:
             for _ in range(nb_per_template):
                 t = tracks[randrange(0,len(tracks))] if '[t]' in template else None
                 s = artists[randrange(0,len(artists))] if '[s]' in template else None
                 g = genres[randrange(0,len(genres))] if '[g]' in template else None
-                fill_slot(X,POS,Intent,intent, template,t=t,s=s,g=g)
+                p = playlist_names[randrange(0,len(playlist_names))] if '[p]' in template else None
+                fill_slot(X,POS,Intent,intent, template,t=t,s=s,g=g, p=p)
 
 
 
         #elif 'List' not in intent and 'list' not in intent:
         #    fill_slot(X,POS,Intent,intent,template)
-
-
     io_utils.dump_to_file(X,POS,Intent,args_output, mode='a')
 
 
@@ -102,6 +105,8 @@ def sent_gen(args):
         genre_list=json.load(f)
     data_sent = pd.read_csv(args.template)
     intent = splitext(basename(args.template))[0]
+
+    playlist_names = pd.read_csv(args.playlist_names)
     print intent
     
     #data_sent = data_sent[data_sent.columns[0]] # remove timestamp column
@@ -109,7 +114,7 @@ def sent_gen(args):
 
     ### select Intent: Given [singer | album | date | track | genre ] find songs
     ### given_row =  data_sent[data_sent.columns[0]] == 'Given'
-    fill_template(data_artist,data_sent,genre_list,args.output, intent,nb_per_template=args.nb_per_template)
+    fill_template(data_artist,data_sent,genre_list, playlist_names, args.output, intent,nb_per_template=args.nb_per_template)
 
 if __name__ == '__main__':
     args = opt_parse()
